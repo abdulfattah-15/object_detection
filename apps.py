3473dfc1d397dc638749aa5dbe39469f72ec7025
abdulfattah-15 <fattahismail1510@gmail.com>
2024-02-01 10:22:33 +0700

    first commit

---
+++ apps.py
@@ -0,0 +1,145 @@
+# Import required libraries
+import PIL
+import cv2
+import streamlit as st
+from ultralytics import YOLO
+from streamlit_webrtc import VideoTransformerBase, WebRtcMode,webrtc_streamer
+import math
+# Replace the relative path to your weight file
+model_path = 'Weights/yolov8n.pt'
+model = YOLO(model_path)
+
+
+class VideoTransformer(VideoTransformerBase):
+    def __init__(self):
+        self.i = 0
+
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        results = model(img, stream=True)
+        i =self.i+1
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+                # put box in cam
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                # confidence
+                confidence = math.ceil((box.conf[0]*100))/100
+                print("Confidence --->",confidence)
+                # class name
+                cls = int(box.cls[0])
+                print("Class name -->", classNames[cls])
+
+                # object details
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+
+                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+        return img
+# object classes
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+# Setting page layout
+st.set_page_config(
+    page_title="Object Detection",  # Setting page title
+    page_icon="🤖",     # Setting page icon
+    layout="wide",      # Setting layout to wide
+    initial_sidebar_state="expanded"    # Expanding sidebar by default
+)
+
+# Creating sidebar
+with st.sidebar:
+    option = st.sidebar.selectbox("Pilih opsi yang ada ..", ("Upload Image", "Capture from Camera"))
+    if option == "Upload Image":
+        st.header("Image/Video Config")     # Adding header to sidebar
+        # Adding file uploader to sidebar for selecting images
+        source_img = st.file_uploader(
+            "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+
+        # Model Options
+        confidence = float(st.slider(
+            "Select Model Confidence", 25, 100, 40)) / 100
+       
+    elif option == "Capture from Camera":
+        
+        st.write("Camera Capture Mode")
+# Creating main page heading
+st.title("Object Detection")
+
+# Creating two columns on the main page
+col1, col2 = st.columns(2)
+if option == "Upload Image":
+    # Adding image to the first column if image is uploaded
+    with col1:
+        if source_img:
+            # Opening the uploaded image
+            uploaded_image = PIL.Image.open(source_img)
+            # Adding the uploaded image to the page with a caption
+            st.image(source_img,
+                    caption="Uploaded Image",
+                    use_column_width=True
+                    )
+
+    try:
+        model = YOLO(model_path)
+    except Exception as ex:
+        st.error(
+            f"Unable to load model. Check the specified path: {model_path}")
+        st.error(ex)
+
+    if st.sidebar.button('Detect Objects'):
+        res = model.predict(uploaded_image,
+                            conf=confidence
+                            )
+        boxes = res[0].boxes
+        res_plotted = res[0].plot()[:, :, ::-1]
+        with col2:
+            st.image(res_plotted,
+                    caption='Detected Image',
+                    use_column_width=True
+                    )
+            try:
+                with st.expander("Detection Results"):
+                    for box in boxes:
+                        st.write(box.xywh)
+            except Exception as ex:
+                st.write("No image is uploaded yet!")
+                
+elif option == "Capture from Camera":
+    try:
+        model = YOLO(model_path)
+    except Exception as ex:
+        st.error(
+            f"Unable to load model. Check the specified path: {model_path}")
+        st.error(ex)
+    
+    webrtc_ctx = webrtc_streamer(
+    key="object-detection",
+    mode=WebRtcMode.SENDRECV,
+    # rtc_configuration={
+    #     "iceServers": get_ice_servers(),
+    #     "iceTransportPolicy": "relay",
+    # },
+    video_transformer_factory=VideoTransformer,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
+    )   
+        
